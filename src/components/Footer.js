95c348c5c1b195d82b451451fff06564b57d9740
suiko <ivanudev@gmail.com>
2020-05-02 05:03:21 +0200

    A dormirla

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,39 @@
 import React from "react";
 import logoFooter from "../images/logo-footer.png";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import ClosedCaptionIcon from "@material-ui/icons/ClosedCaption";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-upper-container">
-        <div className="footer-logo">
-          <img src={logoFooter} alt="bright-ai-logo" />
-        </div>
+        <img src={logoFooter} alt="bright-ai-logo" className="footer-logo" />
         <div className="footer-separator"></div>
-        <div className="footer-redes-container">
-          <div className="red"></div>
-          <div className="red"></div>
-          <div className="red"></div>
-          <div className="red"></div>
+        <div className="footer-icons-container">
+          <a href="#github">
+            <GitHubIcon fontSize="large" />
+          </a>
+          <a href="#twitter">
+            <TwitterIcon fontSize="large" />
+          </a>
+          <a href="#face">
+            <FacebookIcon fontSize="large" />
+          </a>
+          <a href="#insta">
+            <InstagramIcon fontSize="large" />
+          </a>
         </div>
       </div>
-      <div className="footer-final"></div>
+      <div className="footer-final">
+        <p>Bright AI</p>
+        <div className="footer-final-icon">
+          <ClosedCaptionIcon />
+        </div>
+        <p>Open Source Project</p>
+      </div>
     </footer>
   );
 }
